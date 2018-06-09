14c22b07c0a3f30274bb352fb7d6125c0592d968
Diogo Bemfica <diogo.fragabemfica@gmail.com>
2018-06-09 19:53:13 -0300

    melhorias nos logs para debug

--- app.js
+++ app.js
@@ -6,16 +6,19 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 io.on('connection', function(socket){
-    socket.on('nlp',function(msg){
-        console.log('nlp: '+ msg+' <'+Date()+'>');
-        socket.broadcast.emit('nlp',msg);
+    socket.on('nlp',function(response){
+        console.log('nlp: <'+Date()+'>');
+        console.log(response);
+        socket.broadcast.emit('nlp',response);
     });
     socket.on('kernel',function(response){
-        console.log('kernel: '+ response+' <'+Date()+'>');
+        console.log('kernel: <'+Date()+'>');
+        console.log(response);
         socket.broadcast.emit('kernel',response);
     });
     socket.on('client',function(response){
-        console.log('client: '+ response+' <'+Date()+'>');
+        console.log('client: <'+Date()+'>');
+        console.log(response);
         socket.broadcast.emit('client',response);
     });
 });
